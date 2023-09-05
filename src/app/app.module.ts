901fde9044be275cb64085c8704d0d2803acc7e6
OliverMG27 <oliver27jean@gmail.com>
2023-09-05 09:47:50 -0600

    componente menu nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
 AppComponent,
 LoginComponent,
-
-  ],
+MenuComponent
+],
   imports: [
     BrowserModule,
     AppRoutingModule
