7dacfb214608c1fea2037aa536a7d11c123497cc
OliverMG27 <oliver27jean@gmail.com>
2023-09-06 09:58:51 -0600

    ruteo componentes nuevos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { ReporteConsumosComponent } from './reporte-consumos/reporte-consumos.component';
+import { ReporteMortalidadesComponent } from './reporte-mortalidades/reporte-mortalidades.component';
+import { ReportePesadasComponent } from './reporte-pesadas/reporte-pesadas.component';
+import { ReportecierreGranjasComponent } from './reportecierre-granjas/reportecierre-granjas.component';
 
 const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   { path: 'menu', component: MenuComponent},
-
+  { path: 'consumos', component: ReporteConsumosComponent},
+  { path: 'mortalidades', component: ReporteMortalidadesComponent},
+  { path: 'pesadas', component: ReportePesadasComponent},
+  { path: 'cgranjas', component: ReportecierreGranjasComponent},
 ];
 
 
